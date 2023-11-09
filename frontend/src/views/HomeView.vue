7b58ccf4a0c80288cb9bac1a0f13d9545d78ccc5
netoxdw <neto.guevara.tapia@gmail.com>
2023-11-08 16:25:47 -0800

    Iniciando proyecto Vue

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -7,7 +8,6 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-import '../assets/tailwind.css'
 
 export default {
   name: 'HomeView',
